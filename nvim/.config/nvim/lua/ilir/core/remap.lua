87d06d9757c861dc62f2a342f0c67595d0158dce
Ilir Gusija <78830837+ilirgusija@users.noreply.github.com>
2024-03-26 14:18:26 -0400

    Some quality of life changes

--- nvim/.config/nvim/lua/ilir/core/remap.lua
+++ nvim/.config/nvim/lua/ilir/core/remap.lua
@@ -1,6 +1,5 @@
-
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -53,4 +52,5 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+vim.o.foldmethod = 'indent'
 
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,11 +1,22 @@
 # aliases
 alias e='$EDITOR'
-alias cat='bat'
+alias se='sudo $EDITOR'
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    # Commands to run on macOS
+    echo "Running on macOS"
+    # Add macOS-specific aliases, functions, or exports here
+    alias cat='bat'
+else
+    # Commands to run on Linux
+    echo "Running on Linux"
+    # Add Linux-specific aliases, functions, or exports here
+    alias cat='batcat'
+fi
+
 alias ls='eza'
 alias ll='eza -alh'
 alias tree='eza --tree'
-eval "$(zoxide init zsh)"
-alias cd='z'
+#alias cd='z'
 alias vim='nvim'
 eval $(thefuck --alias)
 
@@ -16,3 +27,11 @@ alias gl='git pull'
 
 # ssh
 alias apple='ssh user3@130.15.101.67'
+
+# apple specific aliases
+alias proj='~/projects/ilir/nonlinear_transform_coder/src/'
+
+# terminal specific key bindings
+bindkey '^[f' forward-word
+bindkey "^[b" backward-word
+bindkey "^[^?" backward-kill-word
